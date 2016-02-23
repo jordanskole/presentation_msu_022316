44dadd36f7890fd71bfe031ed55b50d7565267c7
Jordan Skole <jordan@usebenchmark.com>
2016-02-23 13:29:25 -0500

    Update 2016-02-23T18:29:11.977Z

---
+++ Gulpfile.js
@@ -0,0 +1,34 @@
+/**
+ *  Welcome to your gulpfile!
+ *  The gulp tasks are splitted in several files in the gulp directory
+ *  because putting all here was really too long
+ */
+
+'use strict';
+
+var gulp = require('gulp');
+var ghPages = require('gulp-gh-pages');
+var browserSync = require('browser-sync').create();
+
+// Static server
+gulp.task('serve', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    var sources = ["**/*.html", "*.html", "./js/*.js", "./css/**.css"]
+
+    gulp.watch(sources)
+      .on("change", browserSync.reload);
+});
+
+/**
+ *  So cool.
+ *  Push everything up to gh-pages
+ */
+gulp.task('deploy', function() {
+  return gulp.src('**/*')
+    .pipe(ghPages({push: true}));
+});
